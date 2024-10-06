e01c7fc770a6bb3a03e11bbc13a682aa268ef0f6
MayRosario12 <162092946+MayRosario12@users.noreply.github.com>
2024-10-06 22:47:50 +0800

    Add files via upload

--- gradeAssignment_v2.js
+++ gradeAssignment_v2.js
@@ -1,1 +1,28 @@
+// Grade assignment using Conditional Ternary statements
+let grade = score >= 97 ? "1.00" :
+  score >= 94 ? "1.25" :
+  score >= 91 ? "1.50" :
+  score >= 88 ? "1.75" :
+  score >= 85 ? "2.00" :
+  score >= 82 ? "2.25" :
+  score >= 79 ? "2.50" :
+  score >= 76 ? "2.75" :
+  score >= 75 ? "3.00" :
+  score >= 72 ? "4.00" :
+    "5.00";
 
+    //Remarks assignment using If..else if..else Statements
+    let remarks = "";
+    if (score >= 90) {
+        remarks = "HIGH PASS, Candidate for Cum Laude";
+    } else if (score >= 80) {
+        remarks = "AVERAGE PASS";
+    } else if(score >= 75) {
+        remarks = "LOW PASS";
+    } else {
+        remarks = "FAILED, Needs Improvement";
+
+    }
+
+    console.log('You equivalent Grade is "${grade}"');
+    console.log('Your remarks: ${remarks}');
